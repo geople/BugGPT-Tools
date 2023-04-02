4e03f2eed7e235792c271a830c08279a590e975a
Ajam <AjamX101@gmail.com>
2023-04-02 18:13:06 +0545

    Massive Updates

--- linky/linky.sh
+++ linky/linky.sh
@@ -32,23 +32,30 @@ fi
 if [[ "$*" == *"-help"* ]] || [[ "$*" == *"--help"* ]] || [[ "$*" == *"help"* ]] ; then
   echo "➼ Usage: linky -u <url> -o /path/to/outputdir -gh <github_token> -h <optional Headers>"
   echo ""
-  echo "Extended Help"
-  echo "-u,     --url            Specify the URL to scrape (Required)"
-  echo "-o,     --output_dir     Specify the directory to save the output files (Required)"
-  echo "-gh,    --github_token   Specify manually: ghp_xxx (Not Required if $HOME/.config/.github_tokens exists)"
-  echo "-d,     --deep           Specify if Gospider, Hakrawler, Katana & XnLinkfinder should run with depth 5. (Super Slow)"
-  echo "-h,     --headers        Specify additional headers or cookies to use in the HTTP request (optional)"
-  echo "-init,  --init           Initialize ➼ linky by dry-running it against example.com (Only run on a fresh Install)"
-  echo "-up,    --update         Update linky"
-  echo "-ctmp,  --clean-tmp      Cleans /tmp/ files after run"
-  echo "-curls, --clean-urls     Removes noisy junk urls (godeclutter | urless)"
+  echo -e "${YELLOW}Extended Help${NC}"
+  echo "-u,       --url              Specify the URL to scrape (Required)"
+  echo "-o,       --output_dir       Specify the directory to save the output files (Required)"
+  echo "-gh,      --github_token     Specify manually: ghp_xxx (Not Required if $HOME/.config/.github_tokens exists)"
+  echo "-d,       --deep             Specify if Gospider, Hakrawler, Katana & XnLinkfinder should run with depth 5.(Slow)"
+  echo "-h,       --headers          Specify additional headers or cookies to use in the HTTP request (optional)"
+  echo "-init,    --init             Initialize ➼ linky by dry-running it against example.com (Only run on a fresh Install)"
+  echo "-up,      --update           Update linky"
+  echo "-ctmp,    --clean-tmp        Cleans /tmp/ files after run"
+  echo "-curls,   --clean-urls       Removes noisy junk urls (godeclutter | urless)"
+  echo "-params,  --discover-params  Runs Arjun for parameter discovery (Basic & Slow)"
+  echo "-secrets, --scan-secrets     Runs gf-secrets + TruffleHog (Massive Output, Resource-Intensive & Slow)"
   echo ""
-  echo "Example Usage: "
-  echo 'linky --url https://example.com --output_dir /path/to/outputdir --github_token ghp_xyz --headers "Authorization: Bearer token; Cookie: cookie_value"'
+  echo -e "${YELLOW}Example Usage${NC}: "
+  echo -e "${BLUE}Basic${NC}: "
+  echo 'linky --url https://example.com --output_dir /path/to/outputdir --github_token ghp_xyz'
   echo ""
-  echo "Tips: "
+  echo -e "${RED}Exstensive${NC}: "
+  echo 'linky --url https://example.com --output_dir /path/to/outputdir --github_token ghp_xyz --headers "Authorization: Bearer token; Cookie: cookie_value" --deep --discover-params --scan-secrets'
+  echo ""
+  echo -e "${GREEN}Tips${NC}: "
   echo "➼ Include API keys in $HOME/Tools/waymore/config.yml to find more links"
   echo "➼ Include multiple github_tokens in $HOME/.config/.github_tokens to avoid rate limits"
+  echo "➼ '--scan-secrets produces massive files (Several GBs). So TuffleHog is run by default. Best run with --deep" 
   exit 0
 fi
 # Update. Github caches take several minutes to reflect globally  
@@ -137,13 +144,22 @@ do
     -curls|--clean-urls) 
      clean_urls=1
      shift
-    ;;      
+    ;;  
+    -params|--discover-params)
+     discover_params=1
+     shift
+    ;;   
+    -secrets|--scan-secrets)
+     scan_secrets=1
+     shift
+    ;;
     *) 
     echo -e "${RED}Error: Invalid option '$key' , try --help for Usage$(rm -rf $outputDir 2>/dev/null)"
     exit 1
     ;;
   esac
 done
+
 #Setup Vars & default values
 export url=$url
 export outputDir=$outputDir
@@ -157,7 +173,10 @@ fi
 export optionalHeaders=$optionalHeaders
 export deep=$deep
 export clean_tmp=$clean_tmp
+export discover_params=$discover_params
+export scan_secrets=$scan_secrets
 originalDir=$(pwd)
+
 #Recheck Values
 echo -e "${YELLOW}url: ${BLUE}$url${NC}"
 echo -e "${YELLOW}outputDir: ${BLUE}$outputDir${NC}"
@@ -178,8 +197,17 @@ if [ -n "$clean_urls" ] && [ "$clean_urls" -eq 1 ]; then
 else
   echo -e "${YELLOW}Clean URLs (Urless | GoDeclutter)? : ${RED}No $(echo -e "${RED}\u2717${NC}")${NC}"
 fi
-
-# Check if parallel and chromium-chromedriver are installed, and install them if not
+if [ -n "$discover_params" ] && [ "$discover_params" -eq 1 ]; then
+  echo -e "${YELLOW}Parameter Discovery for all URLS? : ${BLUE}Yes $(echo -e "${GREEN}\u2713${NC}")${NC}"
+else
+  echo -e "${YELLOW}Parameter Discovery for all URLS? : ${RED}No $(echo -e "${RED}\u2717${NC}")${NC}"
+fi
+if [ -n "$scan_secrets" ] && [ "$scan_secrets" -eq 1 ]; then
+  echo -e "${YELLOW}Secrets Scanning everything in $outputDir? : ${BLUE}Yes $(echo -e "${GREEN}\u2713${NC}")${NC}"
+else
+  echo -e "${YELLOW}Secrets Scanning everything in $outputDir? : ${RED}No $(echo -e "${RED}\u2717${NC}")${NC}"
+fi
+#Dependency Checks
 if ! command -v chromium >/dev/null 2>&1; then
     echo "➼ chromium-chromedriver is not installed. Installing..."
     sudo apt-get update && sudo apt-get install chromium chromium-chromedriver chromium-common chromium-driver -y
@@ -211,7 +239,7 @@ if ! command -v pipx &> /dev/null; then
    python3 -m pipx ensurepath
 fi
 #Health Check for binaries
-binaries=("anew" "arjun" "fasttld" "fget" "gau" "godeclutter" "gospider" "hakrawler" "js-beautify" "katana" "roboxtractor" "scopegen" "scopeview" "subjs" "unfurl" "waybackurls")
+binaries=("anew" "arjun" "fasttld" "fff" "fget" "gau" "godeclutter" "gospider" "hakrawler" "js-beautify" "katana" "nuclei" "roboxtractor" "scopegen" "scopeview" "subjs" "trufflehog" "unfurl" "waybackurls" "yataf")
 for binary in "${binaries[@]}"; do
     if ! command -v "$binary" &> /dev/null; then
         echo "➼ Error: $binary not found"
@@ -220,28 +248,40 @@ for binary in "${binaries[@]}"; do
         pipx install -f "git+https://github.com/s0md3v/Arjun.git" --include-deps
         go install -v github.com/lc/gau/v2/cmd/gau@latest
         sudo wget https://raw.githubusercontent.com/Azathothas/BugGPT-Tools/main/linky/assets/fasttld -O /usr/local/bin/fasttld && sudo chmod +xwr /usr/local/bin/fasttld
+        go get -u -v github.com/tomnomnom/fff
         GO111MODULE=on && go get -u -v github.com/bp0lr/fget
         go install -v github.com/c3l3si4n/godeclutter@main
         go install -v github.com/jaeles-project/gospider@latest
         go install -v github.com/hakluke/hakrawler@latest
         sudo npm -g install js-beautify
         go install -v github.com/projectdiscovery/katana/cmd/katana@latest
+        go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest && nuclei -update-templates
         go env -w GO111MODULE="auto" ; go get -u github.com/Josue87/roboxtractor
         go install -v github.com/Azathothas/BugGPT-Tools/scopegen@main
         sudo wget https://raw.githubusercontent.com/Azathothas/BugGPT-Tools/main/scopeview/scopeview.sh -O /usr/local/bin/scopeview && sudo chmod +xwr /usr/local/bin/scopeview
         go install -v github.com/lc/subjs@latest
+        cd /tmp && git clone https://github.com/trufflesecurity/trufflehog.git ; cd /tmp/trufflehog && go install -v
         go install -v github.com/tomnomnom/unfurl@latest
         go install -v github.com/tomnomnom/waybackurls@latest
+        go install -v github.com/Damian89/yataf@master
     fi
 done
 #Health Check for Tools
-paths=("$HOME/Tools/JSA/automation.sh" "$HOME/Tools/Arjun/arjun/db/large.txt" "$HOME/Tools/github-search/github-endpoints.py" "$HOME/Tools/urless/urless.py" "$HOME/Tools/waymore/waymore.py" "$HOME/Tools/xnLinkFinder/xnLinkFinder.py")
+paths=("$HOME/Tools/JSA/automation.sh" "$HOME/Tools/Arjun/arjun/db/large.txt" "$HOME/Tools/gf-secrets/gf-secrets.sh" "$HOME/Tools/github-search/github-endpoints.py" "$HOME/Tools/urless/urless.py" "$HOME/Tools/waymore/waymore.py" "$HOME/Tools/xnLinkFinder/xnLinkFinder.py")
 for path in "${paths[@]}"; do
     if [ ! -f "$path" ]; then
         echo "➼ Error: $path not found"
         echo "➼ Attempting to Install missing tools under $HOME/Tools $(mkdir -p $HOME/Tools)"    
         #Arjun
         cd $HOME/Tools && git clone https://github.com/s0md3v/Arjun.git   
+        #Setup gf-patterns
+         cd /tmp/ && git clone https://github.com/NitinYadav00/gf-patterns ; cd /tmp/gf-patterns && mv *.json ~/.gf
+         cd $HOME/Tools && git clone https://github.com/dwisiswant0/gf-secrets 
+         chmod +x $HOME/Tools/gf-secrets/gf-secrets.sh
+        #Using Secres-DB & secpat2gf
+         pip3 install secpat2gf
+         wget https://raw.githubusercontent.com/mazen160/secrets-patterns-db/master/db/rules-stable.yml -O /tmp/rules.yaml 
+         secpat2gf --save -r /tmp/rules.yaml        
         #gwen001/github-search
         cd $HOME/Tools && git clone https://github.com/gwen001/github-search.git && cd $HOME/Tools/github-search && pip3 install -r requirements.txt
         #w9w/JSA
@@ -259,6 +299,7 @@ for path in "${paths[@]}"; do
         python3 $HOME/Tools/xnLinkFinder/setup.py install        
     fi
 done
+
 #Extract root domain name 
 scope_domain=$(echo "$url" | unfurl apexes)
 alt_scope_domain=$(fasttld extract $url | grep -E 'domain:|suffix:' | awk '{print $2}' | sed -n '2,3p' | sed -n '1p;2p' | tr '\n' '.' | sed 's/\.$//' ; echo)
@@ -271,6 +312,7 @@ echo $scope_domain | scopegen -in | anew -q $outputDir/.scope
 echo $alt_scope_domain | scopegen -in | anew -q $outputDir/.scope
 echo -e "${YELLOW}$(cat $outputDir/.scope)${NC}"
 echo ""
+
 #Start Tools
 #Gau
 echo -e "➼ ${YELLOW}Running ${BLUE}gau${NC} on: $url" && sleep 3s
@@ -381,9 +423,9 @@ else
 fi
 
 #JavaScript enum
-cat $outputDir/urls.txt | grep -aEi "\.js([?#].*)?$" | anew $outputDir/js.txt
-#Using JSA: 
-
+#Get JS URLs
+cat $outputDir/urls.txt | grep -aEi "\.(js|jsx|ts|vue|coffee|mjs|es|es6|jsxm|gs|litcoffee|map|svelte|tsx)([?#].*)?$" | anew $outputDir/js.txt
+#Main Module
 echo "➼ Downloading all JS files [fGET] $(mkdir -p $outputDir/jsfiles)"
 cat $outputDir/js.txt | fget -o $outputDir/jsfiles --random-agent --verbose --workers 50
 mv $outputDir/jsfiles/results/**/**/** $outputDir/jsfiles
@@ -400,14 +442,55 @@ cp /tmp/$domain-jsfile-links.txt $outputDir/jsfile-links.txt && cp /tmp/$domain-
 cat $outputDir/jsfile-links.txt | cut -d'[' -f1 | anew $outputDir/endpoints.txt
 cat $outputDir/jsfile-links.txt | grep -aEo 'https?://[^ ]+' | sed 's/]$//' | anew $outputDir/tmp/urls.txt
 cat $outputDir/jsfiles-params.txt | anew $outputDir/parameters.txt 
+#Yataf for JS
+rm -rf /tmp/yataf ; rm -rf $outputDir/Secrets/yataf
+path_to_directory="$outputDir/jsfiles/" ; log_directory="/tmp/yataf" ; mkdir -p "$log_directory"
+find "$path_to_directory" -type f -exec sh -c 'yataf -file "$1" 2>&1 | sed -E '\''s/'$(echo -e "\033")'\[[0-9]{1,2}(;([0-9]{1,2})?)?[mK]//g'\'' | tee -a "$2/$(basename "$1").yataf.log"' _ {} "$log_directory" \;
+# Copy Results and Filter
+find "$log_directory" -type f -iname "*.yataf.log" -exec grep -q "\[i\] No results found" {} \; -delete
+cp -r /tmp/yataf "$outputDir/Secrets/yataf" && clear
 
 #Endpoints
-cat $outputDir/urls.txt | sed '$!N; /^\(.*\)\n\1$/!P; D'| grep -P '\.php|\.asp|\.js|\.jsp|\.jsp' | anew -q $outputDir/endpoints.txt
-sed -i 's#^/##' $outputDir/endpoints.txt
-sed -i '/^https\?:\/\//d' $outputDir/endpoints.txt
+cat $outputDir/urls.txt | sed '$!N; /^\(.*\)\n\1$/!P; D'| grep -P '\.(asp|aspx|bin|cfg|cfm|cgi|coffee|conf|cshtml|dll|eot|es|exe|go|gs|ini|java|js|jsa|jsp|jspx|jsx|jsxm|litcoffee|mjs|mvn|pl|php|ps1|py|rb|sh|sql|toml|ts|vue|wadl|wsdl|xml|yml|yaml|zsh)' | anew -q $outputDir/tmp/all-endpoint-urls.txt
+cat $outputDir/tmp/all-endpoint-urls.txt | unfurl paths | sed 's#^/##' | sort -u -o $outputDir/endpoints.txt
+
+#Secrets
+if [ -n "$scan_secrets" ]; then
+#Download Responses
+ mkdir -p $outputDir/Secrets/fff-urls
+ cat $outputDir/urls.txt | fff --header 'Authorization: Bearer null' --save-status 200 --save-status 405 --save-status 401 --save-status 403 -o $outputDir/Secrets/fff-urls
+#gf-Secrets
+ cd $outputDir && $HOME/Tools/gf-secrets/gf-secrets.sh | tee -a $outputDir/Secrets/gf-secrets.txt 
+#Trufflehog
+ trufflehog filesystem --directory=$outputDir/ --concurrency 70 | tee -a $outputDir/Secrets/trufflehog.txt && clear
+else
+ mkdir -p $outputDir/Secrets/fff-urls
+ cat $outputDir/urls.txt | fff --header 'Authorization: Bearer null' --save-status 200 --save-status 405 --save-status 401 --save-status 403 -o $outputDir/Secrets/fff-urls
+ trufflehog filesystem --directory=$outputDir/ --concurrency 70 | tee -a $outputDir/Secrets/trufflehog.txt && clear
+ echo "" && clear
+fi
 
 #Parameters
+TMPPARAM=$(mktemp)
 cat $outputDir/urls.txt | grep -Po '(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)' | tr -d '?' | tr -d '&' | sed 's/=.*//' | sort -u | uniq | anew -q $outputDir/parameters.txt
+sort -u $outputDir/parameters.txt -o $outputDir/tmp/tmp-param.txt
+cat $outputDir/tmp/tmp-param.txt | grep -v '%' | grep -v '/' | grep -v '_' | grep -v '-' | grep -v '+' | grep -E '.*[0-9].*[0-9].*' | tee "$TMPPARAM"
+comm -23 <(sort $outputDir/tmp/tmp-param.txt) "$TMPPARAM" > $outputDir/tmp/tmp-parameters_filtered.txt
+cat $outputDir/tmp/tmp-parameters_filtered.txt | grep -E '\b\w{10,}\b'| grep  '+' | anew -q $outputDir/tmp/ftmp-param.txt
+comm -23 <(sort $outputDir/tmp/tmp-parameters_filtered.txt) $outputDir/tmp/ftmp-param.txt > $outputDir/tmp/ftmp-parameters_filtered.txt
+mv $outputDir/tmp/ftmp-parameters_filtered.txt $outputDir/parameters.txt && rm -rf $outputDir/tmp/ftmp*.txt $outputDir/tmp/tmp*.txt 
+#URLs with params
+cd $HOME/Tools/urless && python3 $HOME/Tools/urless/urless.py --input $outputDir/urls.txt -o $outputDir/tmp/param-urless.txt && cd -
+cat $outputDir/urls.txt | godeclutter | anew -q $outputDir/tmp/param-urless.txt   
+cat $outputDir/tmp/param-urless.txt | grep -E 'https?://\S+/\S+\.\w+(\?\S+)?' | grep -vE 'https?://\S+/\S+\.js(\?[^[:space:]]*)?$' | sort -u -o $outputDir/tmp/param-urls.txt
+#Arjun
+if [ -n "$discover_params" ]; then
+   cat $outputDir/parameters.txt | grep -E '^[[:alnum:]]+$' && cat $HOME/Tools/Arjun/arjun/db/small.txt | sort -u -o $outputDir/tmp/tmp-arjun-param.txt
+   arjun -i $outputDir/tmp/param-urls.txt -w $outputDir/tmp/tmp-arjun-param.txt -c 250 -t 300 -oT $outputDir/parameters-arjun.txt
+   rm $outputDir/tmp/tmp-arjun-param.txt 
+else
+  echo ""
+fi
 
 #QOL Changes
 find $outputDir -type f -size 0 -delete && find $outputDir -type d -empty -delete
